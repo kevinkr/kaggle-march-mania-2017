eaaf67bd3abc6d5b5d1b1e815eece06e269c93aa
kevinkr <kevinkr@yahoo.com>
2017-02-17 06:25:25 -0600

    new

--- Code/basic-lm-pred-v1.R
+++ Code/basic-lm-pred-v1.R
@@ -1,6 +1,7 @@
 # Kaggle March Learning Mania 2017
 # start 2-10-17
-# Jared Cross model from 2016
+# Jared Cross model from 2016 (https://www.kaggle.com/jaredcross/march-machine-learning-mania-2016/getting-started)
+#
 library(data.table)
 library(dplyr)
 library(reshape2)
@@ -32,3 +33,31 @@ games.to.predict <- cbind(SampleSubmission$id, colsplit(SampleSubmission$id,
                           "_", names = c('season', 'team1', 'team2')))   
 head(games.to.predict)
 
+# Joining games with team seed
+temp <- left_join(games.to.predict, TourneySeeds, by=c("season"="Season", "team1"="Team"))
+games.to.predict <- left_join(temp, TourneySeeds, by=c("season"="Season", "team2"="Team"))
+colnames(games.to.predict)[c(1,5:6)] <- c("Id", "team1seed", "team2seed")
+games.to.predict <- games.to.predict %>% mutate(team1seed = as.numeric(team1seed), team2seed = as.numeric(team2seed))
+
+# Joining (compact) Results with Team Seeds
+temp <- left_join(as.data.frame(TourneyCompactResults), TourneySeeds, by=c("Season", "Wteam"="Team"))
+compact.results <- left_join(temp, TourneySeeds, by=c("Season", "Lteam"="Team"))
+head(compact.results)
+
+# Every win for one team is a loss for the other team…
+set1 <- compact.results %>% select(SeedNum.x, SeedNum.y) %>% mutate(result=1)
+set2 <- compact.results %>% select(SeedNum.y, SeedNum.x) %>% mutate(result=0)
+colnames(set1) <- c("team1seed", "team2seed", "team1win")
+colnames(set2) <- c("team1seed", "team2seed", "team1win")
+full.set <- rbind(set1, set2)
+full.set <- full.set %>% mutate(team1seed = as.numeric(team1seed), team2seed = as.numeric(team2seed))
+
+# Building a Simple Linear Model Based on the Difference in Team Seeds
+m.seed.diff <- lm(team1win~ I(team2seed-team1seed), data=full.set)
+summary(m.seed.diff)
+
+# Making Predictions using the Team Seeds Model
+games.to.predict$Pred <- predict(m.seed.diff, games.to.predict)
+write.csv(games.to.predict %>% select(Id, Pred), 'Submissions/simple-linear-model-diff-team-seeds-v1-2-13-17.csv', row.names=FALSE)
+
+# 2-13-17 PL 0.579107
